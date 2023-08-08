3a66367e8ac9d1e6c2a7d12bc38b271bbbb27782
gusdev0258 <16326176760@edu.udesc.br>
2023-08-08 18:07:15 -0300

    feat: add assets

--- floras-bakery-app/src/App.tsx
+++ floras-bakery-app/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './view/components/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    </>
   )
 }
 
---
+++ floras-bakery-app/src/view/components/Header.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { styled } from "styled-components";
+
+import { ReactComponent as Logo } from "../../assets/logo/logo-floras.svg";
+
+
+const StyledHeader = styled.header`
+  height: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  margin: 0.5rem 2rem;
+`
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+const NavBar = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+  font-size: 1.125rem;
+  line-height: 2rem;
+`
+const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  list-style: none;
+  padding: 0.5;
+  margin: 0;
+  gap: 0.875rem;
+`
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #015314;
+  font-weight: 500;
+`
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <LogoContainer><Logo /></LogoContainer>
+      <NavBar>
+        <NavList>
+          <li><Link href="#">Home</Link></li>
+          <li><Link href="#">Sobre nós</Link></li>
+          <li><Link href="#">Produtos</Link></li>
+          <li><Link href="#">Club</Link></li>
+          <li><Link href="#">Contato</Link></li>
+        </NavList>
+      </NavBar>
+    </StyledHeader>
+  )
+}
+
+export default Header
